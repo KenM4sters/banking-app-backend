d67d3e8ec48d293a1b65028e81f52415e8dd7183
KenM4sters <EHCKenway@outlook.com>
2024-02-08 22:15:10 +0000

    init :zap:

---
+++ src/test/java/com/practice/servingemail/ServingemailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.servingemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServingemailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
